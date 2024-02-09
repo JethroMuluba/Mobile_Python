856c4fea2b90af4db8f125c9b7d6ce6ddad6ceb5
JethroMuluba <jethro.muluba@gmail.com>
2024-02-09 15:48:37 +0100

    Feat: Import all fils on main.kv

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivy.lang import Builder
 Window.size = (370, 700)
 
+class WindowManager(ScreenManager):
+    pass
+
 class MainApp(MDApp):
     # Build est le point d'entrer de l'application
     def build(self):
-        
+        manager = WindowManager()
+        return manager
         # Lier l'élément graphique à Python
-        return Builder.load_file("main.kv")
+        #return Builder.load_file("main.kv")
 app=MainApp()
 app.run()
