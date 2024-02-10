e30c362edde5c418685b038d53bae6f4327804da
JethroMuluba <jethro.muluba@gmail.com>
2024-02-10 11:54:00 +0100

    Feat: link loginSreen and loginKv, also possibility to navigate at StudentsList

--- User/Login/Login_Screen.py
+++ User/Login/Login_Screen.py
@@ -2,5 +2,21 @@ from kivymd.uix.screen import MDScreen
 
 
 class LoginScreen(MDScreen):
-    def on_enter(self, *args):
+    name = ""
+    password = ""
+    def tape_username(self,keyboard_name):
+        self.name = keyboard_name
+        print(self.name)
+
+    def tape_password(self,keyboard_password):
+        self.password = keyboard_password
+        print(self.password)
+
+
+    def connexion(self):
+        print("Form Send", self.name, self.password)
+        self.manager.current = "student"
+
+    def on_enter(self,*args):
         pass
+
