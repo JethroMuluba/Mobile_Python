3670e7b1fbe8ee43eb89cfefddd44ca0c000bdff
JethroMuluba <jethro.muluba@gmail.com>
2024-02-10 09:56:36 +0100

    Feat: add image and title in login page

--- User/Intro/Intro_Screen.py
+++ User/Intro/Intro_Screen.py
@@ -1,6 +1,8 @@
+from kivy.clock import Clock
 from kivymd.uix.screen import MDScreen
 
 
 class IntroScreen(MDScreen):
     def on_enter(self, *args):
+        #Clock.schedule_once(self.goToLoginScreen, 4.0)
         pass
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 from kivymd.app import MDApp
 from kivy.lang import Builder
 Window.size = (370, 700)
 
 class WindowManager(ScreenManager):
-    pass
+    def initial_route(self):
+        self.transition = NoTransition()
+        self.current = "login"
 
 class MainApp(MDApp):
     # Build est le point d'entrer de l'application
     def build(self):
         manager = WindowManager()
+        manager.initial_route()
         return manager
         # Lier l'élément graphique à Python
         #return Builder.load_file("main.kv")
